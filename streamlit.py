70c55ae9fe4514c137e1c95690128b27c86f9ea7
SABHEEN AMMAD <sabheen_ammad@hotmail.com>
2023-10-31 21:30:06 -0700

    4

--- streamlit.py
+++ streamlit.py
@@ -9,31 +9,22 @@ from tensorflow.keras.models import model_from_json
 model_json_file = "Emotion-model.json"
 model_weights_file = "FacialExpression_weights.hdf5"
 
-with open(model_json_file, "r") as json_file:
-    loaded_model_json = json_file.read()
-    model = model_from_json(loaded_model_json)
-model.load_weights(model_weights_file)
 
-# Load the Haar Cascade classifier for face detection
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-# Define the list of emotion labels
-emotion_labels = ["Angry", "Disgust", "Fear", "Happy", "Neutral", "Sad", "Surprise"]
+cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 class VideoProcessor:
 	def recv(self, frame):
 		frm = frame.to_ndarray(format="bgr24")
 
-		faces = face_cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
+		faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
 
 		for x,y,w,h in faces:
 			cv2.rectangle(frm, (x,y), (x+w, y+h), (0,255,0), 3)
 
 		return av.VideoFrame.from_ndarray(frm, format='bgr24')
 
-
-
-
-
-webrtc_streamer(key="example", video_processor_factory=VideoProcessor, rtc_configuration=RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}))
-
+webrtc_streamer(key="key", video_processor_factory=VideoProcessor,
+				rtc_configuration=RTCConfiguration(
+					{"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+					)
+	)
