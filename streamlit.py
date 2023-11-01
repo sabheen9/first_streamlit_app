b4dca45626f4d97315d4e5ba7a7feaad0117ee32
SABHEEN AMMAD <sabheen_ammad@hotmail.com>
2023-10-31 21:24:57 -0700

    4

--- streamlit.py
+++ streamlit.py
@@ -33,13 +33,7 @@ class VideoProcessor:
 
 
 
-st.set_page_config(page_title="Facial Expression Recognition", page_icon=":mango:")
-st.title("Facial Expression Recognition with Streamlit")
 
-with st.sidebar:
-    st.header("Facial Expression Recognition")
-    st.title("Facial Expression Recognition Prediction")
-    st.subheader("Facial expression recognition enables more natural and intuitive interactions between humans and computer systems, enhancing user experience and engagement.")
 
 webrtc_streamer(key="example", video_processor_factory=VideoProcessor, rtc_configuration=RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}))
 
