fdb10d54627aef67ac1b2b05669e97bff913e939
SABHEEN AMMAD <sabheen_ammad@hotmail.com>
2023-10-31 20:37:50 -0700

    4th

--- streamlit.py
+++ streamlit.py
@@ -1,8 +1,34 @@
 from streamlit_webrtc import webrtc_streamer, RTCConfiguration
 import av
+import streamlit as st
 import cv2
+import numpy as np
+from tensorflow.keras.models import model_from_json
 
+
+# Load the pre-trained model
+model_json_file = "Emotion-model.json"
+model_weights_file = "FacialExpression_weights.hdf5"
+
+with open(model_json_file, "r") as json_file:
+    loaded_model_json = json_file.read()
+    model = model_from_json(loaded_model_json)
+model.load_weights(model_weights_file)
+
+# Load the Haar Cascade classifier for face detection
 cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+# Define the list of emotion labels
+emotion_labels = ["Angry", "Disgust", "Fear", "Happy", "Neutral", "Sad", "Surprise"]   
+
+
+
+st.set_page_config(page_title="Facial Expression Recognition", page_icon=":mango:")
+st.title("Facial Expression Recognition with Streamlit")
+
+with st.sidebar:
+    st.header("Facial Expression Recognition")
+    st.title("Facial Expression Recognition Prediction")
+    st.subheader("Facial expression recognition enables more natural and intuitive interactions between humans and computer systems, enhancing user experience and engagement.")
 
 class VideoProcessor:
 	def recv(self, frame):
