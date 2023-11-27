7ddfb2facecb35d559db06d23cf4664b5f43fbd5
SABHEEN AMMAD <sabheen_ammad@hotmail.com>
2023-11-27 00:51:30 -0800

    10

--- streamlit.py
+++ streamlit.py
@@ -18,7 +18,8 @@ model.load_weights(model_weights_file)
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # Define the list of emotion labels
-emotion_labels = ["Angry", "Disgust", "Fear", "Happy", "Neutral", "Sad", "Surprise"]
+#emotion_labels = ["Angry", "Disgust", "Fear", "Happy", "Neutral", "Sad", "Surprise"]
+emotions_labels = {0: 'Angry', 1: 'Disgust', 2: 'Fear', 3: 'Happy', 4: 'Sad',5:'Surprise', 6:'Neutral'} 
 
 class VideoProcessor:
     def recv(self, frame):
